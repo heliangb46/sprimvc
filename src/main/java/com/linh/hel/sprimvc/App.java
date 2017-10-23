f6b8986f860c78fbd2706ab74a9096255e2a9b77
heliang <114846108@qq.com>
2017-10-23 13:02:34 +0800

    for learning springmvc
    base

---
+++ src/main/java/com/linh/hel/sprimvc/App.java
@@ -0,0 +1,13 @@
+package com.linh.hel.sprimvc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
